81ac97802307fd5a6a40e3ce528ed3a626a1335a
张驰 <moyu_zc@163.com>
2021-06-26 13:57:54 +0800

    deploy

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+
+# 确保脚本抛出遇到的错误
+set -e
+
+
+# 部署到自定义域域名
+echo 'www.moyuzc.cn' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 部署到 https://<USERNAME>.github.io
+#git push -f git@github.com:MoYu-zc/MoYu-zc.github.io.git master
+
+git push -f https://github.com/MoYu-zc/MoYu-zc.github.io.git master
+
+# 部署到 https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:MoYu-zc/MoYu-zc.github.io.git master:gh-pages
